3e90efaddb265f441bb88690d9625a75c20bd4ad
Paul Keller <paul.keller.it@gmail.com>
2017-01-09 18:39:02 +0800

    Initial Project Commit
    Creating initial repo

---
+++ FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Positivity365
+//
+//  Created by Paul Keller on 9/1/17.
+//  Copyright © 2017 Paul Keller. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
